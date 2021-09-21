e545a5141d5b712372de828dd153446dc6775bc4
Robin Heidenis <robin.heidenis@gmail.com>
2021-09-21 02:02:43 +0200

    added todos, created stubs for other controllers and routes

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { category, insertQueryResult, searchItem } from "../interfaces";
 import { item, fullItem, itemSequence } from "../interfaces/item.interface";
 import { DBConnect, sequelize } from "./db";
 import { Item } from "./models/item.model";
-import { StandardItem } from "./models/standard_item.model";
+import { StandardItem } from "./models/standardItem.model";
 import { Category } from "./models/category.model";
 import { logger } from "sequelize/types/lib/utils/logger";
 
@@ -28,10 +28,16 @@ app.use(cors());
 app.use(cookieParser());
 
 const itemRouter = require("./routes/item.routes");
+const categoryRouter = require("./routes/category.routes");
+const standardItemRouter = require("./routes/standardItem.routes");
 
+// TODO: incorporate the old api in here, so no breaking changes are forced yet
 app.use("/api/v2/item", itemRouter);
+app.use("/api/v2/category", categoryRouter);
+app.use("/api/v2/standardItem", standardItemRouter);
 
 app.listen(PORT, () => {
+    // TODO: make this a little better, this is not up to standard
     new Item();
     new Category();
     new StandardItem();
