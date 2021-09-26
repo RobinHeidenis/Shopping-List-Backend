6a0ac2df18026da325e4c4bb9065dab8ef8c032e
Robin Heidenis <robin.heidenis@gmail.com>
2021-09-26 17:36:48 +0200

    fixed insecure cors settings by adding origin to options

--- server/index.ts
+++ server/index.ts
@@ -10,11 +10,15 @@ const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const corsOptions = {
+  origin: 'shared.heidenis.com',
+};
+
 app.use(compression());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 const categoryRouter = createRoutes('../controllers/category.controller');
