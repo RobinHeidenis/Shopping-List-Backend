0b9176e5e6acacc682c4a63720930f64d239a2f6
Robin Heidenis <robin.heidenis@gmail.com>
2021-09-29 21:18:01 +0200

    added deleteAll route and controller method for item

--- server/routes/item.routes.ts
+++ server/routes/item.routes.ts
@@ -4,4 +4,5 @@ const controller = require('../controllers/item.controller');
 
 const router = createStandardRoutes('../controllers/item.controller');
 router.post('/sequences', controller.updateSequences);
+router.delete('/all', controller.deleteAllRequest);
 exports.routes = router;
