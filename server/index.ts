86a41488ef8f4c7ed7a001dc5aa7d9532eebc039
Robin Heidenis <robin.heidenis@gmail.com>
2021-10-06 23:43:39 +0200

    Set up session, added events route and controller

--- server/index.ts
+++ server/index.ts
@@ -9,9 +9,39 @@ const compression = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
+const options = {
+  host: process.env.DB_IP,
+  port: 3306,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: 'sessions',
+  createDatabaseTable: true,
+};
+
+const sessionStore = new MySQLStore(options);
+
+const sess = {
+  secret: process.env.accessTokenSecret,
+  store: sessionStore,
+  resave: false,
+  cookie: {
+    secure: false,
+  },
+  saveUninitialized: true,
+};
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
 
 const corsOptions = {
-  origin: 'shared.heidenis.com',
+  origin: '*',
 };
 
 app.use(compression());
@@ -21,7 +51,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
-const itemRouter = require('./routes/item.routes').routes;
+const itemRouter = require('./routes/item.routes');
+
+const searchRouter = require('./routes/search.routes');
+
+const eventsRouter = require('./routes/events.routes');
 
 const categoryRouter = createStandardRoutes('../controllers/category.controller');
 const standardItemRouter = createStandardRoutes('../controllers/standardItem.controller');
@@ -31,4 +65,6 @@ const deprecatedRoutesRouter = require('./routes/deprecated.routes');
 app.use('/api/v2/item', itemRouter);
 app.use('/api/v2/category', categoryRouter);
 app.use('/api/v2/standardItem', standardItemRouter);
+app.use('/api/v2/search', searchRouter);
+app.use('/api/v2/events', eventsRouter);
 app.use('', deprecatedRoutesRouter);
---
+++ server/routes/events.routes.ts
@@ -0,0 +1,11 @@
+export {};
+
+const express = require('express');
+
+const urlRoutes = express.Router();
+
+const controller = require('../controllers/events.controller');
+
+urlRoutes.get('/', controller.events);
+
+module.exports = urlRoutes;
